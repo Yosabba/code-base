3cca771fad59bb713ecf5d9f87f61211be5c2b6b
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-05-14 23:30:38 -0400

    :sparkles: added sidebar with new tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 //Components and pages
 import Home from "./pages/Home";
 import Nav from "./components/Nav";
+import PcGames from "./pages/platforms/PC";
+import Playstation from "./pages/platforms/Playstation";
+import Xbox from "./pages/platforms/Xbox";
+import Nintendo from "./pages/platforms/Nintendo";
+import Ios from "./pages/platforms/Ios";
+import Web from "./pages/platforms/Web";
 //Router
 import { Route, Redirect } from "react-router-dom";
 
@@ -8,8 +14,26 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Route path={["/game/:id", "/"]}>
+      <Route path={["/game/:id", "/"]} exact>
         <Home />
+      </Route>
+      <Route path="/pc" exact>
+        <PcGames />
+      </Route>
+      <Route path="/playstation" exact>
+        <Playstation />
+      </Route>
+      <Route path="/xbox" exact>
+        <Xbox />
+      </Route>
+      <Route path="/nintendo" exact>
+        <Nintendo />
+      </Route>
+      <Route path="/ios" exact>
+        <Ios />
+      </Route>
+      <Route path="/web" exact>
+        <Web />
       </Route>
       <Redirect to="/" />
     </div>
